f220abbbd59be2aa335b3ed77e5dde44a78e862e
Omkar Pawar <pawarom097@gmail.com>
2022-06-10 17:15:41 +0530

    zerovaega

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Blogs } from './blogs/blogs.component';
+import { CardComponent } from './card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: CardComponent },
+  { path: "details", component: Blogs }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { Blogs } from './blogs/blogs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    Blogs
   ],
   imports: [
     BrowserModule,
